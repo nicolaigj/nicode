414f480d0becb1b011c1f725ccd09069a1b7a0bc
hans <hkmelaa@hotmail.com>
2021-11-16 20:59:33 +0100

    feat: add counting and style

--- svelte/src/lib/projects/ProjectsView.svelte
+++ svelte/src/lib/projects/ProjectsView.svelte
@@ -8,7 +8,9 @@
 
 <ul>
 	{#each projects as project}
-		<Project {project} />
+		<li>
+			<Project {project} />
+		</li>
 	{/each}
 </ul>
 
@@ -17,5 +19,33 @@
 		display: flex;
 		flex-direction: column;
 		gap: 2em;
+
+		counter-reset: projex;
+	}
+	li {
+		position: relative;
+		padding: 1em;
+		border: 2px solid var(--color-accent);
+		border-radius: 4px;
+		display: flex;
+		flex-direction: column;
+		font-size: 0.9em;
+	}
+
+	/* counting */
+	li::after {
+		position: absolute;
+		counter-increment: projex;
+		content: '0' counter(projex);
+		right: 0.5em;
+		top: -0.75em;
+		font-size: 2em;
+		text-align: right;
+		padding: 0 0.5em;
+		background-color: var(--color-secondary);
+		color: var(--color-accent);
+	}
+	li:nth-of-type(n + 10):after {
+		content: counter(projex);
 	}
 </style>
