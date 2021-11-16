0babe20a6e2be125b79b12d14967318a4d811288
hans <hkmelaa@hotmail.com>
2021-11-16 20:59:54 +0100

    refactor: move li out of "project"

--- svelte/src/lib/projects/fragments/Project.svelte
+++ svelte/src/lib/projects/fragments/Project.svelte
@@ -16,24 +16,14 @@
 	let { title, period, description, tags } = project;
 </script>
 
-<li>
-	<Period {period} />
-	<h3>
-		{title}
-	</h3>
-	<p>{description}</p>
-	<Tags {tags} />
-</li>
+<Period {period} />
+<h3>
+	{title}
+</h3>
+<p>{description}</p>
+<Tags {tags} />
 
 <style>
-	li {
-		padding: 1em;
-		border: 1px solid var(--color-accent);
-		border-radius: 6px;
-		display: flex;
-		flex-direction: column;
-		font-size: 0.9em;
-	}
 	p {
 		margin: 1em 0;
 	}
