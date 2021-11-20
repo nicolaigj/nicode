9989385ae6b340b21473e2c1bec01c69e4b51d37
hans <hkmelaa@hotmail.com>
2021-11-20 04:23:11 +0100

    fix: less shitty layout

--- svelte/src/lib/intro/fragments/IntroText.svelte
+++ svelte/src/lib/intro/fragments/IntroText.svelte
@@ -1,20 +1,70 @@
 <script>
 	export let consultant;
 
-	$: intro = consultant.intro;
+	$: ({ preamble, email, mobile, linkedin } = consultant.intro);
 </script>
 
-<p>
-	{@html intro}
-</p>
+<div>
+	<p>
+		{preamble}
+	</p>
+
+	<address>
+		{#if email}
+			<a href="mailto:{email}" aria-label="email">
+				<span>E</span>
+				<span>{email}</span>
+			</a>
+		{/if}
+
+		{#if linkedin}
+			<a href={'https://www.linkedin.com/in/' + linkedin} aria-label="linked in profile">
+				<span>L</span>
+				<span>{linkedin}</span>
+			</a>
+		{/if}
+
+		{#if mobile}
+			<a href="tel:{mobile}" aria-label="mobile phone">
+				<span>M</span>
+				<span>{mobile}</span>
+			</a>
+		{/if}
+	</address>
+</div>
 
 <style>
-	p {
+	address {
+		display: flex;
+		flex-flow: column;
+		gap: 0.1em;
+		font-size: 0.8em;
+		justify-content: space-between;
+	}
+	address a {
+		display: flex;
+		gap: 5px;
+		font-size: 1em;
+	}
+	address a span {
+		padding: 0.2em 0.4em;
+		border-radius: 4px;
+	}
+	address a:hover span:first-of-type {
+		text-decoration: none;
+		background-color: var(--color-accent-cta);
+		color: var(--color-secondary);
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
 		font-size: 1.4em;
 		max-width: 480px;
 	}
 	@media (max-width: 600px) {
-		p {
+		div {
 			max-width: 300px;
 			text-align: justify;
 		}
