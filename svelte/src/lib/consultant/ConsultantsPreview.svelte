066aead3b93ab264753b622411ada32b222e9a44
hans <hkmelaa@hotmail.com>
2021-11-22 04:25:12 +0100

    feat: initial consultant pv

---
+++ svelte/src/lib/consultant/ConsultantsPreview.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let consultants: ConsultantType[];
+</script>
+
+<section>
+	{#each consultants as consultant}
+		<a sveltekit:prefetch href={consultant.shortName}>
+			<span>{consultant.shortName}</span>
+		</a>
+	{/each}
+</section>
+
+<style>
+	a {
+		border-radius: 4px;
+		border: 2px solid var(--color-primary);
+		background-color: var(--color-secondary);
+		text-decoration: none;
+		padding: 20px;
+
+		border: 2px solid var(--color-accent-cta);
+	}
+
+	section {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 1em;
+	}
+</style>
