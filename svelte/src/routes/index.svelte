09314e5979b258464354d9c42e3f6827a6a3c6ea
hans <hkmelaa@hotmail.com>
2021-11-28 15:00:32 +0100

    refactor

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { consultants } from '$lib/db';
-	import ConsultantPreview from '$lib/consultant/ConsultantsPreview.svelte';
 	import ContentArticle from '$lib/wrappers/ContentArticle.svelte';
 </script>
 
@@ -9,5 +8,29 @@
 </svelte:head>
 
 <ContentArticle>
-	<ConsultantPreview {consultants} />
+	<section>
+		{#each consultants as consultant}
+			<a sveltekit:prefetch href={consultant.name}>
+				<span>{consultant.name}</span>
+			</a>
+		{/each}
+	</section>
 </ContentArticle>
+
+<style>
+	a {
+		border-radius: var(--b-radius);
+		background-color: var(--color-background);
+		text-decoration: none;
+		padding: 20px;
+
+		border: var(--b-thickness) solid var(--color-interact);
+	}
+
+	section {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 1em;
+	}
+</style>
