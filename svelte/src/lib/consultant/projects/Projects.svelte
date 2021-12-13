fac935816426c208b1118b6bded6ea96a8ccadeb
hans <hkmelaa@hotmail.com>
2021-12-13 16:17:55 +0100

    fix: remove stuff

--- svelte/src/lib/consultant/projects/Projects.svelte
+++ svelte/src/lib/consultant/projects/Projects.svelte
@@ -24,8 +24,6 @@
 		display: flex;
 		flex-direction: column;
 		margin: 1em 0;
-
-		counter-reset: project;
 	}
 	li + li {
 		margin-top: 4em;
@@ -36,29 +34,5 @@
 		display: flex;
 		flex-direction: column;
 		font-size: 0.9em;
-
-		counter-increment: project;
-	}
-
-	li::after {
-		content: counter(project);
-		position: absolute;
-		left: 0;
-		top: -0.2em;
-		bottom: 0;
-		right: 0;
-
-		margin: auto;
-		width: min-content;
-		height: min-content;
-
-		color: var(--color-background);
-		text-shadow: inset 5px 5px 10px var(--color-accent);
-		font-size: 12em;
-		font-weight: bold;
-
-		opacity: 0.5;
-
-		z-index: -1;
 	}
 </style>
