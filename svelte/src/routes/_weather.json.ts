6c07a7f6d7f9d1fcb0f956f40ac106719d5638ec
hans <hkmelaa@hotmail.com>
2021-12-14 11:34:38 +0100

    fix: disable weather endpoint

---
+++ svelte/src/routes/_weather.json.ts
@@ -0,0 +1,39 @@
+type LocationType = {
+	lat: number;
+	lon: number;
+	altitude: number;
+};
+
+const locations = {
+	bergen: {
+		lat: 60.397076,
+		lon: 5.324383,
+		altitude: 5
+	},
+	oslo: {
+		lat: 59.913869,
+		lon: 10.752245,
+		altitude: 5
+	}
+};
+
+/**
+ * calls api.met
+ * https://api.met.no/weatherapi/locationforecast/2.0/documentation#!/data/get_compact
+ * @param lat
+ * @param lon
+ * @param altitude
+ * @returns {Promise<any>} returns a promise with the weather data
+ */
+const forecast = async ({ lat, lon, altitude }: LocationType): Promise<any> => {
+	return fetch(
+		`https://api.met.no/weatherapi/locationforecast/2.0/compact?altitude=${altitude}&lat=${lat}&lon=${lon}`
+	).then((response) => {
+		return response.json();
+	});
+};
+
+export async function get() {
+	// retrun weather data
+	// const data = await forecast(locations.bergen);
+}
