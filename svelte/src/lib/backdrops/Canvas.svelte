245e43a05d9a6dd4d5af4975401b36111f42479d
hans <hkmelaa@hotmail.com>
2022-01-04 16:30:03 +0100

    fix: sprinklefix

--- svelte/src/lib/backdrops/Canvas.svelte
+++ svelte/src/lib/backdrops/Canvas.svelte
@@ -10,23 +10,22 @@
 
 	const drawHistory = () => {
 		ctx.clearRect(0, 0, W, H);
+		ctx.fillStyle = ctx.strokeStyle = 'rgba(50, 255, 255, 0.0125)';
+		ctx.lineWidth = 3;
+
 		if (history.length < 3) return;
-		// ctx.beginPath();
-		// ctx.moveTo(history[0].x, history[0].y);
-		// for (let i = 1; i < history.length; i++) {
-		// 	ctx.lineTo(history[i].x, history[i].y);
-		// }
-		// ctx.stroke();
-		const { x, y, vx, vy } = history[0];
 		ctx.beginPath();
-		ctx.arc(x, y, Math.abs(vx + vy), 0, Math.PI * 2);
-		ctx.fill();
+		ctx.moveTo(history[0].x, history[0].y);
+		for (let i = 1; i < history.length; i++) {
+			ctx.lineTo(history[i].x, history[i].y);
+		}
+		ctx.stroke();
 	};
 
 	const handleMouseMove = ({ clientX, clientY, movementX, movementY }) => {
 		requestAnimationFrame(drawHistory);
 		history.push({ x: clientX, y: clientY, vx: movementX || 0, vy: movementY || 0 });
-		if (history.length > 500) {
+		if (history.length > 100) {
 			history.shift();
 		}
 	};
@@ -34,14 +33,13 @@
 	onMount(() => {
 		resizeCooldown.execute();
 		ctx = canvas.getContext('2d');
-		ctx.fillStyle = ctx.strokeStyle = 'rgba(50, 255, 255, 0.1)';
 
 		const t = setInterval(() => {
 			if (history.length > 0) {
 				history.shift();
 				requestAnimationFrame(drawHistory);
 			}
-		}, 30);
+		}, 15);
 	});
 </script>
 
